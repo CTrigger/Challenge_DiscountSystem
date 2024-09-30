d743b5165691981a4c1cfa057fb60a4308e79a48
CTrigger <hexadecimal.exe@gmail.com>
2024-09-30 02:14:06 +0100

    Repository simulation

---
+++ FileManager/DataHandler.cs
@@ -0,0 +1,75 @@
+﻿using DataFileIO.Interfaces;
+using DiscountCore;
+using FileManager.Interfaces;
+using System.Runtime.CompilerServices;
+using System.Xml;
+[assembly: InternalsVisibleTo("DiscountSystemChallenge")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+namespace FileManager
+{
+    public class DataHandler : IDataHandler
+    {
+        #region Properties
+        public IEnumerable<DiscountData> DiscountRepository { get; set; }
+
+        private readonly XmlReaderSettings ReaderSettings = new XmlReaderSettings() { IgnoreComments = true, IgnoreWhitespace = true };
+        private readonly XmlWriterSettings WriterSettings = new XmlWriterSettings() { Indent = false };
+        private static string PathFolder = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data");
+        private static string DiscountRepositoryPath = Path.Combine(PathFolder, "DiscountContract.xml");
+
+        private readonly IFileIO _access;
+
+        #endregion
+
+        #region Ctor
+        public DataHandler(IFileIO access)
+        {
+            _access = access;
+            LoadData();
+        }
+        ~DataHandler()
+        {
+            SaveData();
+        }
+        #endregion
+
+        #region Methods
+
+        #region Read
+        internal virtual async Task LoadData()
+        {
+            await LoadData_DiscountContract();
+        }
+        internal async Task LoadData_DiscountContract()
+        {
+            if (!File.Exists(DiscountRepositoryPath))
+            {
+                if (!Directory.Exists(Path.GetDirectoryName(DiscountRepositoryPath)))
+                    Directory.CreateDirectory(Path.GetDirectoryName(DiscountRepositoryPath));
+                IEnumerable<DiscountData> empty = Enumerable.Empty<DiscountData>();
+                byte[] empty_data = _access.ObjectToBytes(empty);
+                await _access.SaveFile(DiscountRepositoryPath, empty_data);
+            }
+            byte[] data = await _access.ReadFile(DiscountRepositoryPath);
+
+            DiscountRepository = data == Array.Empty<byte>() ? Enumerable.Empty<DiscountData>() : _access.BytesToObject<IEnumerable<DiscountData>>(data);
+
+        }
+        #endregion
+
+        #region Write
+        public async Task SaveData()
+        {
+            await SaveData_DiscountContract();
+        }
+        public async Task SaveData_DiscountContract()
+        {
+            byte[] data = _access.ObjectToBytes<IEnumerable<DiscountData>>(DiscountRepository);
+            await _access.SaveFile(DiscountRepositoryPath, data);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
---
+++ FileManager/Interfaces/IDataHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscountCore;
+namespace FileManager.Interfaces
+{
+    public interface IDataHandler
+    {
+        IEnumerable<DiscountData> DiscountRepository { get; set; }
+        Task SaveData();
+        Task SaveData_DiscountContract();
+    }
+}
