680cde8e1c02b10dd246f7df57362ea83408ea83
CTrigger <hexadecimal.exe@gmail.com>
2024-10-02 02:44:17 +0100

    SignalR Client/Requester

--- SignalRClient/Program.cs
+++ SignalRClient/Program.cs
@@ -1,36 +1,55 @@
-﻿using Microsoft.AspNetCore.SignalR.Client;
-using DiscountCore;
-using Microsoft.Extensions.Logging;
+﻿using DiscountCore;
+using Microsoft.AspNetCore.SignalR.Client;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
+        // Build the connection to the SignalR Hub
         var connection = new HubConnectionBuilder()
             .WithUrl("https://localhost:7258/DiscountHub")
             .Build();
+
+        // Event handler for receiving messages
+        connection.On<string>("ReceiveMessage", message =>
+        {
+            Console.WriteLine($"Message from server: {message}");
+        });
+
+        // Event handler for receiving broadcasted discount codes
+        connection.On<IEnumerable<DiscountData>>("BroadcastCodes", discounts =>
+        {
+            Console.WriteLine($"Codes were updated:");
+            foreach (var discount in discounts)
+            {
+                Console.WriteLine($"{discount.Code}");
+            }
+        });
+
+        // Start the connection to the SignalR server
+        Console.WriteLine("Press any key to start");
+        Console.ReadKey();
+        await connection.StartAsync();
+        Console.WriteLine("Connection started.");
+
+        // Infinite loop to keep requesting new codes
         do
         {
-            // Start the connection
-            await connection.StartAsync();
-            Console.WriteLine("Connection started.");
-
-            Console.Write("Number of requests:");
+            Console.Write("Number of requests: ");
             if (UInt16.TryParse(Console.ReadLine(), out UInt16 input))
             {
-                Console.Write("Requesting codes");
-                var discountCodes = await connection.InvokeAsync<IEnumerable<DiscountData>>("GenerateDiscountCodes", input);
-                string list = "Generated codes: \r\n" + string.Join(Environment.NewLine, (from c in discountCodes select c.Code));
-                Console.WriteLine(list);
+                // Invoke the server method to generate codes without expecting a return value
+                Console.WriteLine("Requesting codes...");
+                await connection.InvokeAsync("GenerateCodes", input);
             }
             else
             {
-                Console.WriteLine("Not valid number");
+                Console.WriteLine("Not a valid number");
             }
-
-            // Close the connection
-            await connection.StopAsync();
-            Console.WriteLine("Connection ended.");
         } while (true);
+
+        // Stop the connection when done (this won't be reached in the current structure)
+        await connection.StopAsync();
+        Console.WriteLine("Connection ended.");
     }
 }
